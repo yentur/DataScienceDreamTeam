6d8bfdac8db2e7516fe2a44617485cc75b836218
omer.yentur <omer.yentur@krontech.com>
2024-07-24 13:56:29 +0300

    update

---
+++ Main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+
+
+st.title("🌟 Data Science Dream Team 🌟")
+
+
+st.markdown("""
+## Hoş Geldiniz! 🎉
+
+Data Science Dream Team olarak, makine öğrenmesi algoritmalarının teorisi ve uygulamalarını adım adım öğreniyoruz. İlk olarak **Linear Regression** ile başladık ve burada birçok farklı makine öğrenmesi tekniğini birlikte keşfetmeyi hedefliyoruz. 
+
+### Neden Katılmalısınız?
+
+- **Eğitim:** Her hafta yeni bir makine öğrenmesi algoritmasını öğreniyor ve uyguluyoruz. 
+- **Topluluk:** Aynı ilgi alanlarına sahip insanlarla tanışma ve bilgi alışverişi yapma fırsatı.
+- **Destek:** Sorularınızı sormak ve projelerinizde yardım almak için mükemmel bir ortam.
+- **Kaynaklar:** Geniş bir kaynak havuzuna erişim, rehberler ve örnek projeler.
+
+### Neler Öğreneceksiniz?
+
+- **Makine Öğrenmesi:** Makine Öğrenmesi teori ve uygulama yaparak öğrenme.
+- **Data Visualization:** Veriyi görselleştirmenin ve sunmanın en iyi yolları.
+- **Model Evaluation:** Modellerin performansını değerlendirme ve iyileştirme teknikleri.
+- **Feature Engineering:** Veri setlerinizi daha güçlü hale getirme yöntemleri.
+
+Siz de bizimle birlikte çalışmak ve bu heyecan verici yolculuğa katılmak isterseniz, aşağıdaki bilgileri inceleyin! 😊
+""")
+
+
+st.markdown("""
+### Hedeflerimiz 🎯
+
+* **Eğitim Kaynakları Oluşturmak:** Streamlit kullanarak makine öğrenmesi algoritmalarının teorisini ve uygulamalarını birlikte çalışıp, kapsamlı ve erişilebilir eğitim kaynakları oluşturmayı hedefliyoruz. Bu kaynaklar, hem başlangıç seviyesindeki öğrenciler hem de ileri seviye araştırmacılar için faydalı olacaktır.
+* **Topluluk Oluşturmak:** Benzer ilgi alanlarına sahip kişilerin bir araya gelerek bilgi ve deneyim paylaşımı yapabileceği güçlü bir topluluk oluşturmayı amaçlıyoruz. Bu sayede, katılımcılar hem kendi projelerinde daha hızlı ilerleyebilir hem de başkalarına yardımcı olabilir.
+* **Pratik Deneyim Sağlamak:** Teorik bilginin yanı sıra, katılımcılarımıza pratik projeler ve uygulamalı çalışmalar sunarak gerçek dünya problemlerini çözme becerilerini geliştirmelerine yardımcı olmak istiyoruz.
+* **İnovasyonu Teşvik Etmek:** Yeni fikirlerin ve yaklaşımların keşfedilmesi ve geliştirilmesi için bir ortam sağlamak. Bu sayede, makine öğrenmesi alanında yenilikçi çözümler ve projeler ortaya çıkabilir.
+* **Sürekli Gelişim:** Katılımcılarımızın sürekli olarak kendilerini geliştirebilecekleri ve en yeni teknolojilerden haberdar olabilecekleri dinamik bir öğrenme ortamı sunmak.
+
+Bu hedefler doğrultusunda, makine öğrenmesi alanında hem teorik bilgi hem de pratik beceriler kazanarak büyük bir adım atabilirsiniz!
+""")
+
+st.markdown("""
+### Katılın! 📢
+
+Daha fazla bilgi ve tartışma için Discord kanalımıza katılın! Discord kanalımız, Data Science Dream Team topluluğunun kalbidir. Bu platformda:
+
+- **Sohbet ve Tartışma:** Makine öğrenmesi, veri bilimi, projeler ve daha fazlası hakkında diğer üyelerle sohbet edebilirsiniz.
+- **Soru ve Cevap:** Karşılaştığınız zorluklar ve sorular hakkında hızlıca yanıt alabilir, deneyimlerinizi paylaşabilirsiniz.
+- **Etkinlikler ve Çalıştaylar:** Düzenlediğimiz etkinlikler, webinarlar ve çalıştaylarla sürekli öğrenme fırsatına sahip olabilirsiniz.
+- **Proje İşbirlikleri:** Diğer üyelerle birlikte projeler üzerinde çalışabilir, işbirliği yaparak daha büyük ve etkili sonuçlar elde edebilirsiniz.
+- **Kaynak Paylaşımı:** Faydalı makaleler, rehberler, dersler ve daha fazlasını paylaşabilirsiniz.
+
+Haydi, Data Science Dream Team Discord kanalına katılarak bu dinamik ve destekleyici topluluğun bir parçası olun: [Data Science Dream Team Discord](https://discord.gg/Bf7ncyFM)
+""")
+
+
+
+st.markdown("""
+### Katkıda Bulunanlar 💼
+
+- Onur
+- Ömer
+- Mustafa
+""")
+
+
+st.markdown("""
+Data Science Dream Team olarak, birlikte daha güçlü ve bilgili olacağız. Haydi, keşfetmeye başlayalım! 🚀
+""")
